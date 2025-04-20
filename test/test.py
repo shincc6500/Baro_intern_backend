386aa4305d3ace772009e64b5ef5f7619deaee17
shincc6500 <130081283+shincc6500@users.noreply.github.com>
2025-04-20 13:43:15 +0900

    test : 테스트 사례 추가
    로그인 성공 및 username-password 오류 실패사례,
    회원가입 성공 및 중복 아이디 실패사례,
    JWT 토큰 정상인증, 토큰 미입력 사례 에 대한 테스트

--- test/test.py
+++ test/test.py
@@ -1,8 +1,10 @@
 import pytest
+from datetime import timedelta
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import AccessToken
 
 User = get_user_model()
 
@@ -22,6 +24,10 @@ def test_signup_success(api_client):
     assert response.status_code == status.HTTP_201_CREATED
     assert response.data["username"] == "testuser"
 
+    response2 = api_client.post(url, payload, format='json')
+    assert response2.status_code == status.HTTP_400_BAD_REQUEST
+    assert response2.data['error']['code'] == "USER_ALREADY_EXISTS"
+
 @pytest.mark.django_db
 def test_login_success(api_client):
     user = User.objects.create_user(username="loginuser", email="login@example.com", password="loginpass123")
@@ -33,24 +39,40 @@ def test_login_success(api_client):
     response = api_client.post(url, payload, format='json')
     assert response.status_code == status.HTTP_200_OK
     assert "token" in response.data
+    
+    payload2 = {
+        "username": "loginuser",
+        "password": "loginpass12"
+    }
+    response = api_client.post(url, payload2, format='json')
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.data['error']['code']=="INVALID_CREDENTIALS"
+
+def generate_valid_token(user):
+    access_token = AccessToken.for_user(user)
+    return str(access_token)
+
+def generate_expired_token(user):
+    access_token = AccessToken.for_user(user)
+    access_token.set_exp(lifetime=timedelta(seconds=-1))
+    return str(access_token)
 
 
 @pytest.mark.django_db
-def test_jwt_auth_protected_view(api_client):
-    user = User.objects.create_user(username="jwtuser", email="jwt@example.com", password="jwtpass123")
-    login_url = reverse('login')
-    payload = {
-        "username": "jwtuser",
-        "password": "jwtpass123"
-    }
-    response = api_client.post(login_url, payload, format='json')
-    token = response.data["token"]
+def test_token_not_found(api_client):
+    user = User.objects.create_user(username="loginuser", email="login@example.com", password="loginpass123")
+    url = reverse('protected')
 
-    # 보호된 API 요청
-    protected_url = reverse('protected')
+    #토큰이 없는 경우
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.data["error"]["message"] == "토큰이 없습니다."
+
+    #정상적인 경우
+    token = generate_valid_token(user)    
     api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-    protected_response = api_client.get(protected_url)
-
-    assert protected_response.status_code == status.HTTP_200_OK
-    assert protected_response.data["detail"] == "You are authenticated!"
-
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["message"] == "토큰이 인증 되었습니다."
+ 
+    
