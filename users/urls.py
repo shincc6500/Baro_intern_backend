ee8474e6da723e75fac51ad590b851b65caeb103
shincc6500 <130081283+shincc6500@users.noreply.github.com>
2025-04-20 13:46:38 +0900

    fix : 오타수정
    url 주소에 / 생략된 문제 해결

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
+from django.contrib import admin
 from django.urls import path
+
 from .views import SignupView, LoginView, ProtectedView
 
+
+
+
+
 urlpatterns = [
-    path('signup', SignupView.as_view(), name='signup'),
-    path('login', LoginView.as_view(), name='login'),
-    path('protected/', ProtectedView.as_view(), name='protected')
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('protected/', ProtectedView.as_view(), name='protected'),
+
 ]
